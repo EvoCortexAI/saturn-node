db59950e1587b78459ab4658feb85e5d8b0d3a43
Dmitrii Sukhov <admin@evocortex.ai>
2026-07-29 16:00:26 +0200

    Add stable contract problem codes

--- Sources/SaturnNodeCore/Errors.swift
+++ Sources/SaturnNodeCore/Errors.swift
@@ -1,19 +1,112 @@
+public enum SaturnNodeProblemCode: String, CaseIterable, Codable, Hashable, Sendable {
+    case contractVersionUnsupported = "contract_version_unsupported"
+    case malformedRequest = "malformed_request"
+    case unauthenticated
+    case unauthorized
+    case wrongAudience = "wrong_audience"
+    case wrongWorkload = "wrong_workload"
+    case wrongDeployment = "wrong_deployment"
+    case wrongNode = "wrong_node"
+    case modelNotAllowed = "model_not_allowed"
+    case credentialNotYetValid = "credential_not_yet_valid"
+    case credentialExpired = "credential_expired"
+    case credentialRevoked = "credential_revoked"
+    case credentialReplayed = "credential_replayed"
+    case staleCredentialEpoch = "stale_credential_epoch"
+    case requestLimitExceeded = "request_limit_exceeded"
+    case tokenBudgetExceeded = "token_budget_exceeded"
+    case nodeSaturated = "node_saturated"
+    case requestTimeout = "request_timeout"
+    case cancelled
+    case internalFailure = "internal_failure"
+}
+
 public enum SaturnNodeError: Error, Hashable, Sendable {
     case unsupportedManifestVersion(Int)
     case emptyModelAllowlist
     case duplicateModelIdentifier
     case invalidManifestValue(String)
+    case unsupportedContractVersion(String)
+    case malformedRequest(String)
     case invalidCredentialClaims
     case credentialVerificationUnavailable
+    case unauthenticated
+    case unauthorized
+    case wrongAudience
+    case wrongWorkload
+    case wrongDeployment
     case revokedCredential
+    case replayedCredential
     case wrongNode
     case modelNotAllowed
     case staleCredentialEpoch
     case credentialNotYetValid
     case expiredCredential
     case requestExceedsCredentialLimits
+    case requestBudgetExceeded
+    case tokenBudgetExceeded
     case emptyPrompt
     case invalidOutputLimit
     case invalidRuntimeCapabilities
     case runtimeUnavailable
+    case nodeSaturated
+    case requestTimedOut
+    case cancelled
+    case requestAlreadyTerminal
+
+    public var problemCode: SaturnNodeProblemCode {
+        switch self {
+        case .unsupportedContractVersion:
+            .contractVersionUnsupported
+        case .malformedRequest,
+             .unsupportedManifestVersion,
+             .emptyModelAllowlist,
+             .duplicateModelIdentifier,
+             .invalidManifestValue,
+             .invalidCredentialClaims,
+             .emptyPrompt,
+             .invalidOutputLimit,
+             .invalidRuntimeCapabilities:
+            .malformedRequest
+        case .credentialVerificationUnavailable,
+             .runtimeUnavailable,
+             .requestAlreadyTerminal:
+            .internalFailure
+        case .unauthenticated:
+            .unauthenticated
+        case .unauthorized:
+            .unauthorized
+        case .wrongAudience:
+            .wrongAudience
+        case .wrongWorkload:
+            .wrongWorkload
+        case .wrongDeployment:
+            .wrongDeployment
+        case .wrongNode:
+            .wrongNode
+        case .modelNotAllowed:
+            .modelNotAllowed
+        case .credentialNotYetValid:
+            .credentialNotYetValid
+        case .expiredCredential:
+            .credentialExpired
+        case .revokedCredential:
+            .credentialRevoked
+        case .replayedCredential:
+            .credentialReplayed
+        case .staleCredentialEpoch:
+            .staleCredentialEpoch
+        case .requestExceedsCredentialLimits,
+             .requestBudgetExceeded:
+            .requestLimitExceeded
+        case .tokenBudgetExceeded:
+            .tokenBudgetExceeded
+        case .nodeSaturated:
+            .nodeSaturated
+        case .requestTimedOut:
+            .requestTimeout
+        case .cancelled:
+            .cancelled
+        }
+    }
 }
